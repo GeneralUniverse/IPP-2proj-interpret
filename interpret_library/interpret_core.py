1c19cd70e45140e857f585b0d87c969d23bb9242
Dominik <dominik.klon@outlook.com>
2022-04-14 11:29:00 +0200

    added numeric and logical operations

--- interpret_library/interpret_core.py
+++ interpret_library/interpret_core.py
@@ -4,9 +4,9 @@ def _search_var(name):
             return var
 
     return {
-         "name": "",
-         "type": "",
-         "value": ""
+        "name": "",
+        "type": "",
+        "value": ""
     }
 
 
@@ -48,37 +48,99 @@ class Instruction:
         return args
 
     def perform(self):
-        if self._opc == "PUSHS":
+        arg_num = Instruction.get_number_of_args(self)
+
+        if arg_num == 1:
             arg1 = Instruction._args_to_list(self)[0]
-            Instruction.stack.append(arg1)
 
-        if self._opc == "POPS":
-            arg1 = Instruction._args_to_list(self)[0]
-            stack_vals = Instruction.stack.pop()
+            if self._opc == "PUSHS":
+                Instruction.stack.append(arg1)
 
-            _declare_variable(arg1["content"], stack_vals["type"])
-            _init_variable(arg1["content"], stack_vals["content"])
+            if self._opc == "POPS":
+                stack_vals = Instruction.stack.pop()
 
-        if self._opc == "WRITE":
-            arg1 = Instruction._args_to_list(self)[0]
-            var = _search_var(arg1["content"])
+                _declare_variable(arg1["content"], stack_vals["type"])
+                _init_variable(arg1["content"], stack_vals["content"])
 
-            print(var["value"])
+            if self._opc == "WRITE":
+                var = _search_var(arg1["content"])
 
-        if self._opc == "MOVE":
+                print(var["value"])
+
+            if self._opc == "DEFVAR":
+                _declare_variable(arg1["content"], "")
+
+        if arg_num == 2:
             arg1 = Instruction._args_to_list(self)[0]
             arg2 = Instruction._args_to_list(self)[1]
 
-            var = _search_var(arg1["content"])
+            if self._opc == "MOVE":
+                var = _search_var(arg1["content"])
 
-            _init_variable(arg1["content"], arg2["content"])
+                _init_variable(arg1["content"], arg2["content"])
 
-            if var["type"] == "":
-                var["type"] = arg2["type"]
+                if var["type"] == "":
+                    var["type"] = arg2["type"]
 
-        if self._opc == "DEFVAR":
+        if arg_num == 3:
             arg1 = Instruction._args_to_list(self)[0]
+            arg2 = Instruction._args_to_list(self)[1]
+            arg3 = Instruction._args_to_list(self)[2]
 
-            _declare_variable(arg1["content"], "")
+            # operations with number
+            if self._opc in ("ADD", "SUB", "MUL", "IDIV"):
+                if arg2["type"] != "int" or arg3["type"] != "int":
+                    exit("pocetni operace s ne cisli")
+
+                num1 = int(arg2["content"])
+                num2 = int(arg3["content"])
+
+                if self._opc == "ADD":
+                    result = num1 + num2
+
+                if self._opc == "SUB":
+                    result = num1 - num2
+
+                if self._opc == "MUL":
+                    result = num1*num2
+
+                if self._opc == "IDIV":
+                    if num2 == 0:
+                        exit(57)
+                    result = num1 / num2
+
+                _init_variable(arg1["content"], result)
+
+            # comparing instructions
+            if self._opc in ("LT", "GT", "EQ"):
+                result = "false"
+
+                if self._opc == "LT":
+                    if arg2["content"] < arg3["content"]:
+                        result = "true"
+
+                if self._opc == "GT":
+                    if arg2["content"] > arg3["content"]:
+                        result = "true"
+
+                if self._opc == "EQ":
+                    if arg2["content"] == arg3["content"]:
+                        result = "true"
+
+                _init_variable(arg1["content"], result)
+
+            # logical instructions
+            if self._opc in ("AND", "OR"):  # NOT is in 2 argument section
+                result = "false"
+
+                if self._opc == "AND":
+                    if arg2["content"] == "true" and arg3["content"] == "true":
+                        result = "true"
+
+                if self._opc == "OR":
+                    if (arg3["content"]) == "true" or bool(arg3["content"] == "true"):
+                        result = "true"
+
+                _init_variable(arg1["content"], result)
 
 
